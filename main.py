df79b6974f8d12e9caf6a2945912cc4eafd46e78
phamvietannh <phamvietanh@ashbournecollege.co.uk>
2020-01-28 09:41:53 +0000

    dvd thing

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("hello world")
-print("bye world")
+import pygame
+
+pygame.init()
+width, height = 800,  600
+backgroundColor = 255,  0,  0
+
+screen = pygame.display.set_mode((width, height))
+
+while True:
+	screen.fill(backgroundColor)
+	pygame.display.flip()
