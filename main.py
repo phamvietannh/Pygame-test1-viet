b1f3390fc2234ba374fa449068a50d9abd153f2d
phamvietannh <phamvietanh@ashbournecollege.co.uk>
2020-01-28 09:47:50 +0000

    python2

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ import pygame
 
 pygame.init()
 width, height = 800,  600
+dvdlogoSpeed = [1,1]
 backgroundColor = 255,  0,  0
 
 screen = pygame.display.set_mode((width, height))
 
+dvdLogo = pygame.image.load("dvd-logo-white.png")
+
+dvdLogoRect = dvdLogo.get_rect()
+
 while True:
-	screen.fill(backgroundColor)
-	pygame.display.flip()
+	  screen.fill(backgroundColor)
+
+    screen.blit(dvdLogo, dvdLogoRect)
+
+	  pygame.display.flip()
